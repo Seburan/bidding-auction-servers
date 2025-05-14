899bd7e2d99f7e972fb803fef7f33cfd8b82b7f9
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2025-05-14 17:17:16 +0900

    add resources for testing B&A

---
+++ production/deploy/gcp/terraform/environment/sandcastle/testing/test_GetBidsRawRequest.sh
@@ -0,0 +1,15 @@
+# Setup arguments.
+INPUT_PATH=get_bids_request.json  # Needs to be a valid GetBidsRawRequest
+INPUT_FORMAT=JSON
+BFE_HOST_ADDRESS=dsp-x-dev.bfe.privacy-sandbox-demos-dsp-x.dev  # DNS name of BFE service (Example: dns:///buyer.domain.com)
+#(For local runs services, use: BFE_HOST_ADDRESS=localhost:50051)
+CLIENT_IP=142.251.175.95
+
+# Run the tool with desired arguments.
+./builders/tools/bazel-debian run //tools/secure_invoke:invoke \
+    -- \
+    -target_service=bfe \
+    -input_file="/src/workspace/${INPUT_PATH}" \
+    -input_format=${INPUT_FORMAT} \
+    -host_addr=${BFE_HOST_ADDRESS} \
+    -client_ip=${CLIENT_IP}
---
+++ production/deploy/gcp/terraform/environment/sandcastle/testing/test_SelectAdRequest.sh
@@ -0,0 +1,15 @@
+# copy this file and `select_ad_request.json` at the root of the `bidding-auction-servers `project.
+
+# Setup arguments.
+INPUT_PATH=select_ad_request.json  # Needs to be a valid plaintext in the root of the B&A project (i.e. the path is .../bidding-auction-server/select_ad_request.json)
+SFE_HOST_ADDRESS=ssp-x-dev.sfe.privacy-sandbox-demos-ssp-x.dev  # DNS name of SFE (e.g. dns:///seller.domain.com)
+#(For local services, use: SFE_HOST_ADDRESS=localhost:50053)
+CLIENT_IP=142.251.175.95
+
+# Run the tool with desired arguments.
+./builders/tools/bazel-debian run //tools/secure_invoke:invoke \
+    -- \
+    -target_service=sfe \
+    -input_file="/src/workspace/${INPUT_PATH}" \
+    -host_addr=${SFE_HOST_ADDRESS} \
+    -client_ip=${CLIENT_IP}
